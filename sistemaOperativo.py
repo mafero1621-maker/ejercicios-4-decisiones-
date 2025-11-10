b3c0e7250ee315d509d2934ea28caf2fe837c349
mafero1621-maker <mafero1621@gmail.com>
2025-11-10 11:25:04 -0500

    Add files via upload

---
+++ sistemaOperativo.py
@@ -0,0 +1,10 @@
+#sistemaOperativo
+
+letra = input("Ingrese un carácter (a o i) ")
+
+if letra == 'a':
+    print("Android")
+elif letra == 'i':
+    print("iOS")
+else:
+    print("Opción inválida")
